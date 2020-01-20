69982aeccc995d149f32bf15bde962aec2f9446d
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2020-01-20 10:59:37 +0300

    Implement Smothscroll

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
+    const [navbarLinks, setNavbarLinks] = useState(
+        [
+            {
+                to: "about",
+                text: "About",
+            },
+            {
+                to: "experience",
+                text: "Experience",
+            },
+            {
+                to: "education",
+                text: "Education",
+            },
+            {
+                to: "skills",
+                text: "Skills",
+            },
+            {
+                to: "interests",
+                text: "Interests",
+            },
+            {
+                to: "awards",
+                text: "Awards",
+            },
+        ]
+    )
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a className="navbar-brand js-scroll-trigger" href="#page-top">
@@ -14,24 +44,21 @@ const Navbar = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#experience">Experience</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#education">Education</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#awards">Awards</a>
-                    </li>
+                    {
+                        navbarLinks.map((navbarLink, key) => {
+                            return (
+                                <li className="nav-item" key={ key }>
+                                    <Link 
+                                        activeClass="active"
+                                        to={ navbarLink.to }
+                                        spy={ true }
+                                        smooth={ true }
+                                        // offset={ -70 }
+                                        duration= { 500 }>{ navbarLink.text }</Link>
+                                </li>
+                            );
+                        })
+                    }
                 </ul>
             </div>
         </nav>
