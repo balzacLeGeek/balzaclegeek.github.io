d9db768c97bef81b66e326188fa2840e1c9323c7
RAZAFIMANDIMBY Niaina Michaël (balzacLeGeek) <michaniainar@gmail.com>
2020-06-18 21:03:31 +0300

    Update navbar title on mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-block d-lg-none">Clarence Taylor</span>
+                <span className="d-block d-lg-none">balzacLeGeek</span>
                 <span className="d-none d-lg-block">
                     <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="./imgs/RAZAFIMANDIMBY-Niaina-Michael-profil.jpg" alt="balzacLeGeek"/>
                 </span>
