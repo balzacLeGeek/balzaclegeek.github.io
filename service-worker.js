07235b363afc0ee0338408bc36bca1343dcbf1c6
RAZAFIMANDIMBY Niaina Michaël (balzacLeGeek) <michaniainar@gmail.com>
2020-06-24 15:18:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd80d4c87c5fc38e58e15a4927b7d8a2.js"
+  "/precache-manifest.26076a7effce7fca51d43d3960e520bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
